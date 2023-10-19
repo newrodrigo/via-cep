210e6daae7b8225514c3869b34b095a89d0787b2
newrodrigo <rodrigocerqueira@outlook.com>
2023-10-18 22:19:58 -0300

    Styling for Card List

---
+++ js/controllers/form-controller.js
@@ -0,0 +1,51 @@
+import Address from "../models/address.js";
+
+function State() {
+
+    this.address = new Address();
+
+    this.btnSave = null;
+    this.btnClear = null;
+
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNumber = null;
+    this.inputCity = null;
+
+    this.errorCep = null;
+    this.errorNumber = null;
+}
+
+const state = new State();
+
+export function init() {
+
+    state.inputCep = document.forms.newAddress.cep;
+    state.inputStreet = document.forms.newAddress.street;
+    state.inputNumber = document.forms.newAddress.number;
+    state.inputCity = document.forms.newAddress.city;
+
+    state.btnSave = document.forms.newAddress.btnSave;
+    state.btnClear = document.forms.newAddress.btnClear;
+
+    state.errorCep = document.querySelector('[data-error="cep"]');
+    state.errorNumber = document.querySelector('[data-error="number"]');
+
+    state.inputNumber.addEventListener('change', handleinputNumberChange);
+
+}
+
+function handleinputNumberChange(event) {
+    if(event.target.value == "") {
+        setFormError("number", "Campo requerido");
+    }
+    else {
+        setFormError("number", "");
+    }
+}
+
+function setFormError(key, value) {
+    const element = document.querySelector(`[data-error="${key}"]`);
+    element.innerHTML = value;
+
+}
