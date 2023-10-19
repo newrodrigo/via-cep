b43924a37b865ff4899436d1d3039a88f75fd956
newrodrigo <rodrigocerqueira@outlook.com>
2023-10-18 23:09:29 -0300

    Request service and exceptions

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,4 +1,5 @@
 import Address from "../models/address.js";
+import * as requestService from '../services/request-service.js'
 
 function State() {
 
@@ -32,8 +33,14 @@ export function init() {
     state.errorNumber = document.querySelector('[data-error="number"]');
 
     state.inputNumber.addEventListener('change', handleinputNumberChange);
-
     state.btnClear.addEventListener('click', handleBtnClearClick);
+    state.btnSave.addEventListener('click', handleBtnSaveClick);
+}
+
+async function handleBtnSaveClick(event) {
+    event.preventDefault();
+    const result = await requestService.getJson('https://viacep.com.br/ws/01001000/json/');
+    console.log(result);
 }
 
 function handleinputNumberChange(event) {
